733ee21cb28c27861abf205c1b8d493a2ce15108
Endre Simo <esimovaq@yahoo.com>
2017-08-19 17:23:10 +0300

    Adding CLI tool to generate diagram directly

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
 package main
 
 import (
+	"github.com/esimov/diagram/io"
 	"github.com/esimov/diagram/ui"
+	"github.com/esimov/diagram/canvas"
 	"math/rand"
 	"time"
+	"flag"
+	"log"
+	"os"
+)
+
+var (
+	source		= flag.String("source", "", "Source")
+	destination = flag.String("destination", "", "Destination")
 )
 
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano())
-	ui.InitApp()
+
+	// Generate diagram directly with command line tool.
+	if len(os.Args) > 1 {
+		flag.Parse()
+		input := string(io.ReadFile(*source))
+
+		if err := canvas.DrawDiagram(input, *destination); err != nil {
+			log.Fatal("Error on converting the ascii art to hand drawn diagrams!")
+		}
+	} else {
+		ui.InitApp()
+	}
 }
